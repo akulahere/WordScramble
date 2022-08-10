0bc1156b6678f8197038da8e1b46036faa95eb77
akula <akulinind@gmail.com>
2022-08-10 11:49:30 +0300

    feat: add base logic

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var usedWords = [String]()
+  @State private var rootWord = ""
+  @State private var newWord = ""
+  
+  var body: some View {
+    NavigationView {
+      List {
+        Section {
+          TextField("Enter your word", text: $newWord)
+            .textInputAutocapitalization(.never)
+
+        }
+          
+        Section {
+          ForEach(usedWords, id: \.self) { word in
+            HStack {
+              Image(systemName: "\(word.count).circle")
+              Text(word)
+            }
+          }
+        }
+      }
+      .navigationTitle(rootWord)
+      .onSubmit(addNewWord)
+      .onAppear(perform: startGame)
     }
+  }
+  
+  func addNewWord() {
+    let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+    guard answer.count > 0 else { return }
+    
+    withAnimation {
+      usedWords.insert(answer, at: 0)
+    }
+    usedWords.insert(answer, at: 0)
+    newWord = ""
+  }
+  
+  func startGame() {
+    // 1. Find the URL for start.txt in our app bundle
+    if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+      // 2. Load start.txt into a string
+      if let startWords = try? String(contentsOf: startWordsURL) {
+        // 3. Split the string up into an array of strings, splitting on line breaks
+        let allWords = startWords.components(separatedBy: "\n")
+        
+        // 4. Pick one random word, or use "silkworm" as a sensible default
+        rootWord = allWords.randomElement() ?? "silkworm"
+        // If we are here everything has worked, so we can exit
+        return
+      }
+    }
+    
+    // If were are *here* then there was a problem – trigger a crash and report the error
+    fatalError("Could not load start.txt from bundle.")
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
