05e6c972aed91d408690da92811ae3439177188d
akula <akulinind@gmail.com>
2022-08-11 13:44:05 +0300

    feat: add validation. new game and score

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,6 +11,12 @@ struct ContentView: View {
   @State private var usedWords = [String]()
   @State private var rootWord = ""
   @State private var newWord = ""
+  @State private var errorTitle = ""
+  @State private var errorMessage = ""
+  @State private var showingError = false
+  @State private var score = 0
+
+  
   
   var body: some View {
     NavigationView {
@@ -33,6 +39,22 @@ struct ContentView: View {
       .navigationTitle(rootWord)
       .onSubmit(addNewWord)
       .onAppear(perform: startGame)
+      .alert(errorTitle, isPresented: $showingError) {
+        Button("OK", role: .cancel) { }
+      } message: {
+        Text(errorMessage)
+      }
+    }
+    .toolbar {
+      Button("New Game", action: startGame)
+    }
+    .safeAreaInset(edge: .bottom) {
+      Text("Score: \(score)")
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(.blue)
+        .foregroundColor(.white)
+        .font(.title)
     }
   }
   
@@ -40,30 +62,83 @@ struct ContentView: View {
     let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
     guard answer.count > 0 else { return }
     
+    guard answer.count > 3 else {
+      wordError(title: "Word too short", message: "Words must be at least four letters long.")
+      return
+    }
+    
+    guard answer != rootWord else {
+      wordError(title: "Nice try…", message: "You can't use your starting word!")
+      return
+    }
+    
+    guard isOriginal(word: answer) else {
+      wordError(title: "Word used already", message: "Be more original")
+      return
+    }
+    
+    guard isPossible(word: answer) else {
+      wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+      return
+    }
+    
+    guard isReal(word: answer) else {
+      wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+      return
+    }
     withAnimation {
       usedWords.insert(answer, at: 0)
     }
-    usedWords.insert(answer, at: 0)
+    score += answer.count
     newWord = ""
   }
   
   func startGame() {
-    // 1. Find the URL for start.txt in our app bundle
+    score = 0
+
+    newWord = ""
+    usedWords.removeAll()
     if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
-      // 2. Load start.txt into a string
       if let startWords = try? String(contentsOf: startWordsURL) {
-        // 3. Split the string up into an array of strings, splitting on line breaks
         let allWords = startWords.components(separatedBy: "\n")
-        
-        // 4. Pick one random word, or use "silkworm" as a sensible default
         rootWord = allWords.randomElement() ?? "silkworm"
-        // If we are here everything has worked, so we can exit
         return
       }
     }
+        fatalError("Could not load start.txt from bundle.")
+  }
+  
+  func isOriginal(word: String) -> Bool {
+    !usedWords.contains(word)
+  }
+  
+  func isPossible(word: String) -> Bool {
+    var tempWord = rootWord
     
-    // If were are *here* then there was a problem – trigger a crash and report the error
-    fatalError("Could not load start.txt from bundle.")
+    for letter in word {
+      if let pos = tempWord.firstIndex(of: letter) {
+        tempWord.remove(at: pos)
+      } else {
+        return false
+      }
+    }
+    
+    return true
+  }
+  
+  func isReal(word: String) -> Bool {
+    let checker = UITextChecker()
+    let range = NSRange(location: 0, length: word.utf16.count)
+    let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+    
+    return misspelledRange.location == NSNotFound
+  }
+  
+  func wordError(title: String, message: String) {
+    
+    errorTitle = title
+    errorMessage = message
+    showingError = true
   }
 }
 
